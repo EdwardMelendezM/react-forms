d9b03ee507e81de97749f7572327058319218c36
EdwardMelendezM <192666@unsaac.edu.pe>
2023-04-27 22:07:04 -0500

    adding react-form with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import ReactController from "./components/ReactController";
 
 interface IFormInput {
   firstName: string;
@@ -8,14 +9,23 @@ interface IFormInput {
 
 export default function App() {
   const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    const { firstName, lastName, age } = data
+    console.log("El primer nombre es:", firstName);
+    console.log("El segundo nombre es:", lastName);
+    console.log("La eda es igual a :", age);
+
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i, required: true })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName", { required: true, maxLength: 20 })} />
+        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i, required: true })} />
+        <input type="number" {...register("age", { min: 18, max: 99 })} />
+        <input type="submit" />
+      </form>
+      <ReactController />
+    </div>
   );
 }
